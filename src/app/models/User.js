b96163e7006df5b6e61b465f814a0c330deec465
MicaelCosta <micaelcosta97@hotmail.com>
2019-08-08 20:46:45 -0300

    first commit

---
+++ src/app/models/User.js
@@ -0,0 +1,40 @@
+import Sequelize, { Model } from 'sequelize';
+import bcrypt from 'bcryptjs';
+
+class User extends Model {
+  static init(sequelize) {
+    super.init(
+      {
+        // os campos aqui contidos não necessariamente existem no banco
+        // tratam-se de campos que o usuario pode preencher no envio
+        name: Sequelize.STRING,
+        email: Sequelize.STRING,
+        password: Sequelize.VIRTUAL,
+        password_hash: Sequelize.STRING,
+        provider: Sequelize.BOOLEAN,
+      },
+      {
+        sequelize,
+      }
+    );
+
+    // hook seria equivalente a uma trigger, antes de uma ação será executado a função
+    this.addHook('beforeSave', async user => {
+      if (user.password) {
+        user.password_hash = await bcrypt.hash(user.password, 8);
+      }
+    });
+
+    return this;
+  }
+
+  static associate(models) {
+    this.belongsTo(models.File, { foreignKey: 'avatar_id', as: 'avatar' });
+  }
+
+  checkPassword(password) {
+    return bcrypt.compare(password, this.password_hash);
+  }
+}
+
+export default User;
